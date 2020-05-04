fa435f7294ea62719b08ca3683b7c1dc9f2581ea
frannco94 <63458655+frannco94@users.noreply.github.com>
2020-05-04 12:28:18 -0300

    Add files via upload

--- 01-Adicion/adicion.cpp
+++ 01-Adicion/adicion.cpp
@@ -1,10 +1,18 @@
-
+/*
+01-Adicion
+Franco Ortega
+149.4624-4*/
 
 
 #include <iostream>
 int main (){
-    int a, b;
+    int a,b,suma;
+    std::cout << "Ingrese un numero entero\n";
     std::cin >> a;
+    std::cout << "Ha ingresado el numero " << a << "\n"; 
+    std::cout << "Ingrese un numero entero\n";
     std::cin >> b;
-    std::cout << a + b;
+    std::cout << "Ha ingresado el numero " << b <<"\n";
+    suma=a+b ;
+    std::cout << "La suma es " << suma;
 }
